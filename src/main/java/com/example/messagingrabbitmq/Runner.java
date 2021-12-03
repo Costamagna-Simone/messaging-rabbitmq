1c5e6f192fcd0dd46a88c9e9013d865834beae7e
giovanna <gio.petrone@gmail.com>
2021-12-03 14:16:34 +0100

    first version

--- src/main/java/com/example/messagingrabbitmq/Runner.java
+++ src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -20,7 +20,7 @@ public class Runner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Sending message...");
-		rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ (Gio)!");
+		rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ (Gio Dic 2021)!");
 		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
 	}
 
